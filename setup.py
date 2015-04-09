bda993a1bb07c0f83ba284c8a4dfd2ca0e5bfbfa
vlinhart <vladimir.linhart@gmail.com>
2015-04-09 17:28:35 +0200

    install the templatetags as well

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name = "snippets",
-    version = "150",
+setup(name='snippets',
+    version = "151",
     description = "simple text/html snippet libraries for django",
     author = "Calvin Spealman",
     author_email = "ironfroggy@gmail.com",
     url = "http://github.com/ironfroggy/django-snippets",
-    packages = ['snippets']
+    packages=find_packages()
 )
